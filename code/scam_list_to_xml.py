5410818e9de3a4badc28c2af21b4e25f558195d1
chrismeunier <ch.chrismuller@pm.me>
2025-04-01 18:23:40 +0200

    List creation with version update

--- code/scam_list_to_xml.py
+++ code/scam_list_to_xml.py
@@ -3,11 +3,13 @@ Turn the spam lists (with and without patterns) into a xml file to be imported i
 """
 
 import plistlib
+from pathlib import Path
 from helpers import (
     ROOT_PATH,
     XML_FILE,
     XML_PATTERNS_FILE,
     get_most_recent_txt_file,
+    compare_and_update_files,
 )
 
 
@@ -15,7 +17,7 @@ def write_xml_file(
     xml_filename: str,
     txt_file_glob: str = "spam_numbers*.txt",
     xml_entry_description: str = "Spam",
-):
+) -> Path:
     # xml file creation from list of spam numbers formatted as "+411234567890"
     most_recent_file = get_most_recent_txt_file(filename_pattern=txt_file_glob)
     print(f"File used: {most_recent_file}")
@@ -27,13 +29,15 @@ def write_xml_file(
             for spam_number in text_file
         ]
 
-        dump_plist(xml_filename, plist_body)
+    dump_plist(ROOT_PATH / xml_filename, plist_body)
+    return ROOT_PATH / xml_filename
 
 
-def dump_plist(xml_filename: str, plist_body: list):
-    print(f"Writing in {ROOT_PATH/xml_filename}")
-    with open(ROOT_PATH / xml_filename, "wb") as xml_file:
-        plistlib.dump(plist_body, xml_file)
+def dump_plist(xml_file: Path, plist_body: list):
+    new_xml = xml_file.parent / (xml_file.stem + "_new" + xml_file.suffix)
+    print(f"Writing in {new_xml}")
+    with open(new_xml, "wb") as f:
+        plistlib.dump(plist_body, f)
 
 
 def xml_entry_from_number(number: str, title: str):
@@ -49,7 +53,7 @@ def append_xml_file(
     xml_filename: str,
     txt_file_glob: str = "spam_numbers*.txt",
     xml_entry_description: str = "Spam",
-):
+) -> Path:
     if not (ROOT_PATH / xml_filename).exists():
         raise FileNotFoundError
 
@@ -64,22 +68,26 @@ def append_xml_file(
             for spam_number in text_file
         ]
         plist_body.extend(new_plist_body)
-        dump_plist(xml_filename, plist_body)
+
+    dump_plist(ROOT_PATH / xml_filename, plist_body)
+    return ROOT_PATH / xml_filename
 
 
 def main():
     # write the resulting files to a .xml formatted for Begone
-    write_xml_file(xml_filename=XML_FILE)
-    write_xml_file(
+    new_xml = write_xml_file(xml_filename=XML_FILE)
+    compare_and_update_files(ROOT_PATH / XML_FILE, new_xml)
+    new_xml = write_xml_file(
         xml_filename=XML_PATTERNS_FILE,
         txt_file_glob="pattern_numbers*.txt",
         xml_entry_description="Spam (local.ch++)",
     )
-    append_xml_file(
-        xml_filename=XML_PATTERNS_FILE,
+    new_xml = append_xml_file(
+        xml_filename=new_xml.name,
         txt_file_glob="standalone_numbers*.txt",
         xml_entry_description="Spam (local.ch)",
     )
+    compare_and_update_files(ROOT_PATH / XML_PATTERNS_FILE, new_xml)
 
 
 if __name__ == "__main__":
