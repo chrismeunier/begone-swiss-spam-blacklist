cc4d6e46dccf114b961b612aa058546f74f73024
chrismeunier <ch.chrismuller@pm.me>
2025-04-01 18:01:06 +0200

    Helper functions for file comparison

--- code/helpers.py
+++ code/helpers.py
@@ -1,6 +1,7 @@
 """Helpers and constants for the other scripts"""
 
 import re
+import filecmp
 from pathlib import Path
 from datetime import datetime
 
@@ -31,6 +32,34 @@ def get_most_recent_txt_file(FOLDER: Path = ARCHIVE_PATH, filename_pattern: str 
     text_files = list(FOLDER.glob(filename_pattern))
     return max(text_files, key=lambda f: f.stem.split("_")[-1])
 
+def get_version_file(f: Path):
+    return f.parent / (f.stem + ".version")
+
+def update_version_file(f: Path):
+    version_file = get_version_file(f)
+
+    with open(version_file, "r") as fv:
+        version = fv.readline().strip().split(".")
+        count = fv.readline().strip()
+    major = int(version[0])
+    minor = int(version[1])
+    count = int(count)
+    # Increment
+    version_up = ".".join([str(major), str(minor + 1)])
+    count_up = str(count + 1)
+
+    with open(version_file, "w") as fv_up:
+        fv_up.write(version_up + "\n")
+        fv_up.write(count_up)
+
+def compare_and_update_files(f_old: Path, f_new: Path):
+    if filecmp.cmp(f_old, f_new):
+        f_new.unlink()
+        print(f"No update to the list {f_old.stem}")
+    else:
+        f_new.replace(f_old)
+        update_version_file(f_old)
+        print(f"Updated {f_old.stem}")
 
 if __name__ == "__main__":
     pass
